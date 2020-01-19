33063c85e612fe4112e5d794288e6fac65b14e83
matthewkilloran <mattkgithub@gmail.com>
2020-01-18 23:28:55 -0800

    Added calendar component skeleton

--- scipio-scheduler-application/src/app/app.module.ts
+++ scipio-scheduler-application/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CalendarComponent } from './components/calendar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +8,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
