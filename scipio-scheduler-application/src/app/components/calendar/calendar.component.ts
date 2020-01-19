37fef407ae488563aa8c97fd42ac0e663f3a367e
matthewkilloran <mattkgithub@gmail.com>
2020-01-19 00:23:26 -0800

    Added list of available days to Skips calendar. Fleshed out calendar component architecture.

--- scipio-scheduler-application/src/app/components/calendar/calendar.component.ts
+++ scipio-scheduler-application/src/app/components/calendar/calendar.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'calendar',
-    template: '<h2>{{ getTitleOfPage() }}</h2>'
+    template: `
+    <h2>{{ getTitleOfPage() }}</h2>
+    <ul>
+        <li *ngFor="let i of scipio_availability">
+            {{ i }}
+        </li>
+    </ul>
+    `
 })
 
 export class CalendarComponent {
     title = "The Scipio Calendar!";
+    scipio_availability = ["Tuesday", "Thursday", "Friday"];
 
     getTitleOfPage() {
         return this.title;
